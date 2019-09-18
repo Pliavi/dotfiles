ccfb7039af949fec452936bb0db32eee458cb6be
Pliavi <pliavi.rpgm@gmail.com>
2019-09-18 12:24:15 -0300

    remove unused files

--- .bash_scripts/aliases.sh
+++
@@ -1,4 +0,0 @@
-shopt -s expand_aliases
-
-alias inspire = "node ~./inspiration/inspire.js"
-alias send-inspire = send-notify "Inspiration" $(inspire)
--- .bash_scripts/misc.sh
+++
@@ -1,4 +0,0 @@
-# Set the alt+shift keyboard layout toggle
-# Define os layouts de teclado para alternar
-setxkbmap -layout us,br
-setxkbmap -option 'grp:alt_shift_toggle'
