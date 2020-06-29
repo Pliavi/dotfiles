197bdd10abdb4432eadf109ffccaf6dee92b7df5
pliavi <pliavi.rpgm@gmail.com>
2020-06-29 11:45:11 -0300

    chaging code-oss to vscode-bin in installations

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 #================#
 # Offical repo
 sudo pamac remove --no-confirm firefox
-sudo pamac install --no-confirm yarn zsh micro discord code-oss chromium gimp kdenlive yarn ttf-jetbrains-mono ttf-cascadia-code
+sudo pamac install --no-confirm yarn zsh micro discord chromium gimp kdenlive yarn ttf-jetbrains-mono ttf-cascadia-code
 sudo pamac reinstall noto-fonts-emoji
 sudo fc-cache -f
 fc-cache -f
 
 # AUR
-sudo pamac build --no-confirm spotify-adblock prezto-git insomnia ngrok
+sudo pamac build --no-confirm spotify-adblock prezto-git insomnia ngrok visual-studio-code-bin
 # Took too much time (instalações demoradas)
 sudo pamac build --no-confirm aseprite
 
